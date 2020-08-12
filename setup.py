b661b39f6df9f8511450c08f6ae7e4975fa47ba8
Sreenidhi Vijayaraghavan <sv23@illinois.edu>
2020-08-12 18:13:40 +0400

    First command made

--- setup.py
+++ setup.py
@@ -1,20 +1,29 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix = '.')
+client = commands.Bot(command_prefix = '.')
 
-@bot.event
+
+@client.event
 async def on_ready():
     print("G4G Bot is ready to start.")
 
-#TO DO
-client.run('NzQzMDU5ODkzNzA3MDc5NzIw.XzPKdA.hEcTqwnxHhNRHjmu7aw6hI0MEWQ')
-
-
-@bot.event
+@client.event
 async def on_member_join(member):
     print(f'{member} has joined the server!')
 
-@bot.event
+@client.event
 async def on_member_remove(member):
     print(f'{member} has left the server')
+
+#takes in context as param
+@client.command()
+async def schedule(ctx, *, time, ta_name):
+    await ctx.send(f'Schedule request received.\n Time: {time}\n TA/CA name: {ta_name} \n Position in queue: ')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Latency: {round(client.latency * 1000)} ms')
+
+#TO DO
+client.run('NzQzMDU5ODkzNzA3MDc5NzIw.XzPKdA.rLmHRjoPY5BWxZ25yNAA5PyZ1zY')
